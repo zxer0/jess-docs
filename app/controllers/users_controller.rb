81eb02ff1474b9f58a139b43db04001e99a7c9ea
jrobins <jessrrobins@gmail.com>
2016-03-07 02:27:07 +0000

    user management woo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,35 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
+    def index
+        if current_user.admin?
+            @users = User.all
+        end
+    end
+    
+    def edit
+        @roles = Role.all
+    end
+    
+    def update
+        respond_to do |format|
+          if @user.update(user_params)
+            format.html { redirect_to users_path, notice: 'User was successfully updated.' }
+            format.json { render :show, status: :ok, location: @user }
+          else
+            format.html { render :edit }
+            format.json { render json: @user.errors, status: :unprocessable_entity }
+          end
+        end
+    end
+  
+    private
+        def set_user
+            if current_user.admin?
+                @user = User.find(params[:id])
+            end
+        end
+        
+        def user_params
+          params.require(:user).permit(:role_id)
+        end
 end
